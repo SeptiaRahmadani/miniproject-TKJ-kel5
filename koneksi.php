37d9614cc72664bcbaf91660a0582a0444f5dfea
SeptiaRahmadani <rahmadaniseptiya15@gmail.com>
2026-09-11 09:55:02 +0700

    Establish database connection in koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -3,5 +3,9 @@ $host = "localhost";
 $username = "root";
 $password = "";
 $database = "data_peminjaman_alat";
+$koneksi = mysqli_connect($host, $username, $password, $database);
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
 ?>
 
