727d2ae76abe59c88aa910a9b0438050ae3d77ec
SeptiaRahmadani <rahmadaniseptiya15@gmail.com>
2026-09-16 08:19:56 +0700

    beberapa perubahan di style.css dan koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -4,8 +4,8 @@ $username = "root";
 $password = "";
 $database = "data_peminjaman_alat";
 $koneksi = mysqli_connect($host, $username, $password, $database);
+
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
-
